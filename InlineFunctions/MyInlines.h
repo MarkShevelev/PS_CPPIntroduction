f0c1a5b44d4ae1ff7f6319f421660963a40fcafb
MarkShevelev <shevelev_m@itstep.org>
2019-02-13 12:33:29 +0300

    inline on master

---
+++ InlineFunctions/MyInlines.h
@@ -0,0 +1,13 @@
+#pragma once
+//ќбыкновенно встраиваемые функции определ€ют в заголовочных файлах,
+//т.к. их нет смысла заранее компилировать
+
+inline
+double sqr(double x) {
+	return x * x;
+}
+
+inline 
+double cube(double x) {
+	return x * x;
+}
